bd74a139e6fb6e9dedc8b61c6b36e3e9ba3d091b
ah <gintzopoulos.ggl@gmail.com>
2019-10-20 19:46:02 -0400

    Ajout classe LinkingContext

--- main/include/linking_context.hpp
+++ main/include/linking_context.hpp
@@ -1,13 +1,29 @@
 #pragma once
 
+#include <iostream>
 #include <map>
+#include <iterator>
+#include "game_object.hpp"
+
+
+using idObjet = int;
+using gameObjt = std::shared_ptr<GameObject>;
 
 class LinkingContext
 {
 public:
-
-
+	
+	
 private:
-	//std::map<,> idToObjptr;
-	//std::map<,> objptrToId;
+	std::map<idObjet, gameObjt> idToObjptr;
+	std::map<gameObjt, idObjet> objptrToId;
+	int indice = idToObjptr.size + 1;
+
+	void AddTo_Context(gameObjt);
+
+	void SupprFrom_List(gameObjt);
+
+	void AddTo_List(gameObjt);
+	
+
 };
--- main/src/linking_context.cpp
+++ main/src/linking_context.cpp
@@ -1,1 +1,27 @@
 #include "linking_context.hpp"
+
+
+void LinkingContext::AddTo_Context(gameObjt)
+{
+
+}
+
+void LinkingContext::SupprFrom_List(gameObjt)
+{
+
+}
+
+void LinkingContext::AddTo_List(gameObjt g) {
+	int i = indice;
+	//bool trouve = false;
+	//idToObjptr[i] = g;
+
+	//if (idToObjptr::iterator it = idToObjptr.find(g) {
+	idToObjptr.insert(std::pair<int, gameObjt>(i, g));
+	objptrToId.insert(std::pair<gameObjt, int>(g, i));
+
+	//}
+	
+}
+
+	
