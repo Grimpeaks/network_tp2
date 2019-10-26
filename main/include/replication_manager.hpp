cadfadf48fd199a572a6874dbb82621ce83592f8
Alexandra <alexandra13042307@gmail.com>
2019-10-26 15:50:13 -0400

    fix

--- main/include/replication_manager.hpp
+++ main/include/replication_manager.hpp
@@ -5,19 +5,20 @@
 #include "game_object.hpp"
 #include "linking_context.hpp"
 #include "utils.hpp"
-
+using PacketTypeID = uint32_t;
+enum class PacketType : PacketTypeID {
+	Hello = 0x00,
+	Sync = 0x01,
+	Bye = 0x02,
+	PacketType_Max
+};
 using gameObjt_ptr = std::shared_ptr<GameObject>;
 class ReplicationManager
 {
 public:
 	void Replicate(OutputStream& stream, std::vector<gameObjt_ptr> list_goptr);
 private:
-	enum class PacketType : uint32_t {
-		Hello = 0x00,
-		Sync = 0x01,
-		Bye = 0x02,
-		PacketType_Max
-	};
+	
 	uint32_t m_protocolID = 0x4EDC7489;
 	std::unordered_set<gameObjt_ptr> m_set;
 	LinkingContext m_linkingContext = LinkingContext();
