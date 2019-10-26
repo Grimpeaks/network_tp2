527352c771ce1a8e2c331691d97168bcc128515a
Alexandra <alexandra13042307@gmail.com>
2019-10-26 15:36:41 -0400

    Replicate

--- main/include/replication_manager.hpp
+++ main/include/replication_manager.hpp
@@ -4,14 +4,22 @@
 #include <unordered_set>
 #include "game_object.hpp"
 #include "linking_context.hpp"
+#include "utils.hpp"
 
-using gameObjt = std::shared_ptr<GameObject>;
+using gameObjt_ptr = std::shared_ptr<GameObject>;
 class ReplicationManager
 {
 public:
-	void Replicate(MemoryStream memo, std::vector<gameObjt> list_goptr);
-	void Replicate(MemoryStream memo);
-
+	void Replicate(OutputStream& stream, std::vector<gameObjt_ptr> list_goptr);
 private:
-	std::unordered_set<gameObjt> m_set;
+	enum class PacketType : uint32_t {
+		Hello = 0x00,
+		Sync = 0x01,
+		Bye = 0x02,
+		PacketType_Max
+	};
+	uint32_t m_protocolID = 0x4EDC7489;
+	std::unordered_set<gameObjt_ptr> m_set;
+	LinkingContext m_linkingContext = LinkingContext();
 };
+
--- main/src/replication_manager.cpp
+++ main/src/replication_manager.cpp
@@ -1,11 +1,18 @@
 #include "replication_manager.hpp"
 
-void ReplicationManager::Replicate(MemoryStream stream, std::vector<gameObjt> list_goptr)
-{
-	
-}
-
-void ReplicationManager::Replicate(MemoryStream memo)
-{
+void ReplicationManager::Replicate(OutputStream& stream, std::vector<gameObjt_ptr> list_goptr) {
+	stream.Write(this->m_protocolID);
+	auto type = to_integral(PacketType::Sync);
+	stream.Write(type);
+	for (auto game_objt : list_goptr) {
+		auto objectID = m_linkingContext.GetNetworkId(game_objt);
+		if (!objectID) {
+			continue;
+		}
+		stream.Write(*objectID);
+		auto classID = game_objt->ClassID();
+		stream.Write(classID);
+		game_objt->Write(stream);
+	}
 }
 
