d2349d610957bb5ca9dc53081926a02178fca8aa
Grimpeaks <44480134+Grimpeaks@users.noreply.github.com>
2019-10-28 10:15:48 -0400

    gitflow-hotfix-stash: Correction
    Correction de l'ajout au linkingContext depuis le replication manager

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -40,8 +40,6 @@ int main(int argc, char* argv[])
 		Enemy* enemyPtr = reinterpret_cast<Enemy*>(m_classRegistry->Create('ENEM'));
 		objPtrVector.push_back(reinterpret_cast<GameObject*>(enemyPtr));
 		objPtrVector.push_back(reinterpret_cast<GameObject*>(playerPtr));
-		m_replicationManager.m_linkingContext.AddTo_Context(reinterpret_cast<GameObject*>(enemyPtr));
-		m_replicationManager.m_linkingContext.AddTo_Context(reinterpret_cast<GameObject*>(playerPtr));
 
 		OutputStream streamToSend;
 		m_replicationManager.Replicate(streamToSend, objPtrVector);
@@ -52,7 +50,6 @@ int main(int argc, char* argv[])
 		const auto toDelete = objPtrVector[0];
 		objPtrVector.erase(objPtrVector.begin());
 		m_replicationManager.Replicate(streamToSend, objPtrVector);
-		m_replicationManager.m_linkingContext.SupprFrom_List(toDelete);
 		tcpServer.Send(reinterpret_cast<uint8_t*>(streamToSend.Data().data()), streamToSend.Size());
 	}
 	else
